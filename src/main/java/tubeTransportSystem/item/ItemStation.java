ca72114fd2be1969a4786396e2372a51208ff545
Alz454 <shadeddimensions@gmail.com>
2015-06-19 13:59:38 +0100

    Closes #2

--- src/main/java/tubeTransportSystem/item/ItemStation.java
+++ src/main/java/tubeTransportSystem/item/ItemStation.java
@@ -68,28 +68,6 @@ public class ItemStation extends ItemBlockWithMetadata {
     }
 
     @Override
-    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int p_77648_7_, float p_77648_8_, float p_77648_9_, float p_77648_10_) {
-        boolean supr = super.onItemUse(stack, player, world, x, y, z, p_77648_7_, p_77648_8_, p_77648_9_, p_77648_10_);
-
-        if (supr)
-            if (world.isAirBlock(x, y + 1, z))
-                return true;
-
-        return false;
-    }
-
-    @Override
-    public boolean func_150936_a(World world, int x, int y, int z, int p_150936_5_, EntityPlayer player, ItemStack stack) {
-        boolean supr = super.func_150936_a(world, x, y, z, p_150936_5_, player, stack);
-
-        if (supr)
-            if (world.isAirBlock(x, y + 1, z))
-                return true;
-
-        return false;
-    }
-
-    @Override
     public void registerIcons(IIconRegister iconRegister) {
         super.registerIcons(iconRegister);
         entrance = iconRegister.registerIcon(TubeTransportSystem.MOD_ID + ":station1");
